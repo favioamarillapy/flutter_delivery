0ac6edada4f97238ebee9315219e154a373cbab6
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2023-08-22 15:30:17 -0400

    get current location

--- lib/widgets/home_app_bar.dart
+++ lib/widgets/home_app_bar.dart
@@ -1,9 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:flutter_delivery/widgets/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class CustomAppBar extends StatelessWidget {
+class CustomAppBar extends StatefulWidget {
   const CustomAppBar({super.key});
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  late String cityName = 'Getting location...';
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation();
+  }
+
+  Future<void> _getLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    } else {
+      if (!serviceEnabled) {
+        return Future.error('Location services are disabled.');
+      }
+
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+
+      setState(() {
+        cityName = placemarks.first.locality ?? 'Desconocido';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,9 +59,9 @@ class CustomAppBar extends StatelessWidget {
             color: Colors.white70,
             onPressed: () => Scaffold.of(context).openDrawer(),
           ),
-          const Column(
+          Column(
             children: [
-              Text(
+              const Text(
                 "Location",
                 style: TextStyle(
                   color: Colors.grey,
@@ -29,13 +70,13 @@ class CustomAppBar extends StatelessWidget {
               ),
               Row(
                 children: [
-                  Icon(
+                  const Icon(
                     Icons.map,
                     color: Colors.blueGrey,
                   ),
                   Text(
-                    "Ypacarai, Paraguay",
-                    style: TextStyle(
+                    cityName,
+                    style: const TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 15,
                     ),
